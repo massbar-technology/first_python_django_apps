edba452b4b96c18db37fa7ce94e79bbf83624d7c
massbar-technology <arunm.mehta@gmail.com>
2024-10-08 02:04:37 +0545

    Final commit

--- photoghar/photoghar/urls.py
+++ photoghar/photoghar/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
     path('about/',views.aboutpage),
 
-    path('posts/', include('posts.urls'))
+    path('posts/', include('posts.urls')),
+    path('users/', include('users.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photoghar/posts/urls.py
+++ photoghar/posts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [ 
-    path('', views.posts_list), 
+    path('', views.posts_list, name ='list'), 
+    path('post-new', views.posts_new, name ='post-new'), 
+    path('<slug:slug>', views.posts_page, name ='page'), 
 ]
